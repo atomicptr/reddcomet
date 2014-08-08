a53022a50427818c500f91aa4c66f599b93dd53a
Christopher Kaster <ikasoki@gmail.com>
2014-08-08 17:03:25 +0200

    add active page code

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -1,6 +1,12 @@
 Template.header.helpers({
     islocked: function() {
         return islocked();
+    },
+
+    active_if_template: function(template) {
+        var current_route = Router.current();
+
+        return current_route && template === current_route.lookupTemplate() ? 'active' : '';
     }
 });
 
