c6f92e45ddd89b5a2258c6790dd644081f833e73
Christopher Kaster <ikasoki@gmail.com>
2014-08-08 14:30:45 +0200

    fix syncing helper

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -9,13 +9,30 @@ Template.layout.helpers({
         return true;
     },
 
-    synching: function() {
+    syncing: function() {
         if(Wallet.findOne() && Blocks.findOne()) {
             var wallet = Wallet.findOne();
 
-            var blockcount = Blocks.findOne().blockcount;
+            Meteor.call("get_block_hash", wallet.blocks, function(err, hash) {
+                Meteor.call("get_block", hash, function(err, block) {
+                    Session.set("block_time", block.time);
+                });
+            });
 
-            return wallet.blocks != blockcount;
+            var block_time = Session.get('block_time');
+
+            if(block_time) {
+                var block_date = new Date(block_time * 1000);
+
+                var curr_date = new Date();
+
+                var diff = curr_date.getTime() - block_date.getTime();
+
+                var seconds = Math.abs(diff / 1000);
+
+                return Math.round(seconds / (60 * 60)) > 0;
+
+            }
         }
 
         return false;
